f0ae8b2cab4335e4141ea7efc46e20de5b6ef26f
SweetDream Malaysia <sweetdreammsia@gmail.com>
2024-10-12 00:55:29 +0800

    Add google tag

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -12,6 +12,20 @@
 </script>
 
 <svelte:head>
+	<!-- Google Tag Manager -->
+	<script>
+		(function (w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-W55DQ2HR');
+	</script>
+	<!-- End Google Tag Manager -->
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-1WHBF4BHYB"></script>
 	<script>
